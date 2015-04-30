fa3b04a26a4477843bfccbf9fa68c84b913df0b6
Paul Hines <paul.hines@uvm.edu>
2015-04-30 07:19:08 -0600

    Removed simdc from psoptions.

--- psoptions.m
+++ psoptions.m
@@ -45,9 +45,6 @@ opt.sim.use_control = 0;
 opt.sim.use_comm_model = false;
 opt.sim.dt_max_default = 60; % maximum amount of time between dcsimsep iterations
 opt.sim.simple_redispatch = false; % Simple method used by Zussman's model
-% legacy
-opt.simdc = opt.sim;
-
 
 %% drawing stuff
 opt.draw.width = 0.1;
